56e812ac19e0462917f2060b6af12d3a26bcb9b4
Zhiyu Fang <fang-zhiyu@outlook.com>
2024-02-23 11:43:18 -0500

    feat: responsive agent page

--- src/components/agentscomp/AgentsFirstComp/AgentsFirstComp.js
+++ src/components/agentscomp/AgentsFirstComp/AgentsFirstComp.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import "./AgentsFirstComp.css"
-import AgentsMain from "../../../assets/images/AgentsMain.png";
+import React from "react";
+import "./AgentsFirstComp.css";
+// import AgentsMain from "../../../assets/images/AgentsMain.png";
 export default function AgentsFirstComp() {
   return (
-    <div className='agent-first-main'>
-      <img src={AgentsMain} className='agents-first-image'/>
-       <div className='agent-first-btn'>
-          <span>Let's Talk</span>
-       </div>
+    <div className="agent-first-main">
+      <div className="agent-first-main-text">
+        Hello <span class="agent-first-future-partners">future partners</span>, lets help renters become homeowners
+      </div>
+      <div className="agent-first-btn">
+        <span>Let's Talk</span>
+      </div>
     </div>
-  )
+  );
 }
