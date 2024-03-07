b58958cdf0ff9d1bea9766c12db9209b863f91a6
Zhiyu Fang <fang-zhiyu@outlook.com>
2024-03-07 16:13:21 -0500

    feat: responsive footer

--- src/pages/home/AboutUs.js
+++ src/pages/home/AboutUs.js
@@ -4,14 +4,16 @@ import AboutCompOne from '../../components/aboutcomp/AboutCompOne/AboutCompOne';
 import AboutCompTwo from '../../components/aboutcomp/AboutCompTwo/AboutCompTwo';
 import AboutCompThree from "../../components/aboutcomp/AboutCompThree/AboutCompThree";
 import AboutCompFour from "../../components/aboutcomp/AboutCompFour/AboutCompFour";
+import Footer from '../../components/footer/Footer';
 export default function AboutUs() {
   return (
     <div>
-     {/* <Navbar/> */}
-     <AboutCompOne/>
-     <AboutCompTwo/>
-     <AboutCompThree/>
-     <AboutCompFour/>
+      {/* <Navbar/> */}
+      <AboutCompOne />
+      <AboutCompTwo />
+      <AboutCompThree />
+      <AboutCompFour />
+      <Footer />
     </div>
   )
 }
